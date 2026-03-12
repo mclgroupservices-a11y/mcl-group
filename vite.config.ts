899568d04b7e991c7314ee1c548ab6b72e4bc4ea
mclgroupservices-a11y <mclgroupservices@gmail.com>
2026-03-12 19:38:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/mcl-group/',
+  base: './',
   server: {
     host: "::",
     port: 8080,
